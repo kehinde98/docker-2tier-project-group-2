f2121f9693917c37a97478683ab174ef1f61780b
kehinde98 <makanjuolakehinde98@gmail.com>
2025-10-30 14:28:46 +0100

    Add frontend files to main repo

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello there, this is group 2's application and it's running with Flask!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port=8081)
